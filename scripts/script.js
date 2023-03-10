113213f6300e14d1226b2c1f8989ef7f384a8af4
Kristina Gurachenkova <gurachenkova@yandex.ru>
2023-03-10 12:07:46 +0300

    added mobile version

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,28 @@
-let editButton = document.querySelector(".edit-button");
-let closeButton = document.querySelector(".popup__close-button");
+// Присвоим переменные найденным элементам (кнопка открытия окна редактироваия, кнопка закрытия окна редактирования)
+const editButton = document.querySelector(".edit-button");
+const closeButton = document.querySelector(".popup__close-button");
 
-let popup = document.querySelector(".popup");
+// Выберем непосредственно попап
+const popup = document.querySelector(".popup");
 
-let nameUser = document.querySelector(".profile__info-name").textContent;
-let professionUser = document.querySelector(".profile__info-profession").textContent;
+//Выберем непосредственно форму редактрирования
+const formElement = document.querySelector(".popup__form");
 
+// Присвоем переменным текстовое значение, содержащееся в полях шапки профиля
+const nameUser = document.querySelector(".profile__info-name");
+const professionUser = document.querySelector(".profile__info-profession");
+
+// Переменные для полей попапа
+const nameInput = document.querySelector(".popup__fieldset_name");
+const professionInput = document.querySelector(".popup__fieldset_profession");
+
+// Функция, отвечающая за отображение текста профиля в полях попапа
 function setPopUp(){
-  let nameInput = document.querySelector(".popup__fieldset_name");
-  let professionInput = document.querySelector(".popup__fieldset_profession");
-
-  nameInput.value = nameUser;
-  professionInput.value = professionUser;
+  nameInput.value = nameUser.textContent;
+  professionInput.value = professionUser.textContent;
 }
 
+// Два происходящих события по клику пользователя (открытие и закрытие попапа)
 editButton.addEventListener('click', function(){
   popup.classList.add("popup_opened");
   setPopUp();
@@ -23,8 +32,22 @@ closeButton.addEventListener('click', function(){
   popup.classList.remove("popup_opened");
 });
 
+// Функция, отвечющая за редактирование информации
+function handleFormSubmit (evt) {
+    evt.preventDefault();
 
-//let likeButtons = document.querySelectorAll(".element__like");
+    nameUser.textContent = nameInput.value;
+    professionUser.textContent = professionInput.value;
+
+    popup.classList.remove("popup_opened");
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+
+
+// Функция постановки лайка
+// let likeButtons = document.querySelectorAll(".element__like");
 
 // likeButtons.forEach(button => button.addEventListener('click', function(){
 //   button.classList.replace("element__like", "element__like_active")
