72acb7f7070bb41de064a6aa81ccf44cfb8335e1
Kristina Gurachenkova <gurachenkova@yandex.ru>
2023-05-07 19:34:19 +0600

    full worked

--- index.js
+++ index.js
@@ -5,28 +5,20 @@ import PopupWithForm from './scripts/components/PopupWithForm.js';
 import PopupWithImage from  './scripts/components/PopupWithImage.js';
 import { initialCards } from './scripts/utils/initialCards.js';
 import { popupEditionSelector, popupNewCardSelector, popupPhotoSelector,
-  nameUser, professionUser, nameInput, professionInput, cardsContainerSelector } from './scripts/utils/constants.js';
+  nameUser, professionUser, cardsContainerSelector, buttonAddCard,
+  buttonEditProfile, validationVariables } from './scripts/utils/constants.js';
 import UserInfo from './scripts/components/UserInfo.js';
 import Section from './scripts/components/Section.js';
 
-// Поиск всех необходимых DOM-элементов
-const buttonEditProfile = document.querySelector(".profile__edit-button");
-const buttonAddCard = document.querySelector(".profile__add-button");
 
-// Объект настроек с селекторами
-const validationVariables = {
-  inputSelector: '.popup__input',
-  inputErrorClass: 'popup__input_error',
-  saveButtonSelector: '.popup__save-button',
-  saveButtonDisabledClass: 'popup__save-button_disabled',
-  spanErrorClass: 'popup__error_visible',
-  popupErrorTypeSelector: '.popup__error_type_'
-};
-
+// Запуск отображения данных пользователя со страницы в попап и сохранение при редактировании
+const userInfo = new UserInfo({ name: nameUser, profession: professionUser });
 
 // Вызов функции запуска сабмита формы
 const popupEdition = new PopupWithForm(popupEditionSelector, handleFormEditionSubmit);
 const popupNewCard = new PopupWithForm(popupNewCardSelector, handleNewElement);
+
+Array.of(popupEdition, popupNewCard).forEach(a => a.setEventListeners());
 
 // Функция, отвечющая за редактирование информации
 function handleFormEditionSubmit(evt) {
@@ -47,44 +39,39 @@ const handleCardClick = (data) => {
   popupPhoto.open();
 }
 
+function getCardElement(item) {
+  const newCard = new Card(item, '.gallery', handleCardClick);
+  return newCard.generateCard();
+}
+
+function renderFunc(item, cardSection) {
+  const cardElement = getCardElement(item);
+  cardSection.addItem(cardElement);
+}
+
 // Функция добавления новой карточки пользователем
 function handleNewElement(evt) {
   evt.preventDefault();
   const inputValues = popupNewCard.getInputValues();
   const data = [inputValues];
 
-  const newCardSection = new Section({ items: data, renderer: (item) => {
-    const newCard = new Card(item, '.gallery', handleCardClick);
-    const cardElement = newCard.generateCard();
-    newCardSection.addItem(cardElement);
-}}, cardsContainerSelector)
+  const newCardSection = new Section({ items: data, renderer: (item) => renderFunc(item, newCardSection)}, cardsContainerSelector);
 
   newCardSection.renderAll();
 
   popupNewCard.close();
 };
 
+// Отрисовка карточек
+const baseCardSection = new Section({ items: initialCards, renderer: (item) => renderFunc(item, baseCardSection)}, cardsContainerSelector);
+
+baseCardSection.renderAll();
+
 // Вызов функции запуска валидации формы
 const formEditionValidator = new FormValidator(validationVariables, '.popup__form_type_edition');
 const formNewCardValidator = new FormValidator(validationVariables, '.popup__form_type_new-card');
 
 Array.of(formEditionValidator, formNewCardValidator).forEach(a => a.enableValidation());
-
-
-Array.of(popupEdition, popupNewCard).forEach(a => a.setEventListeners());
-
-// Запуск отображения данных пользователя со страницы в попап и сохранение при редактировании
-const userInfo = new UserInfo({ name: nameUser, profession: professionUser });
-
-// Отрисовка карточек
-const baseCardSection = new Section({ items: initialCards, renderer: (item) => {
-    const newCard = new Card(item, '.gallery', handleCardClick);
-    const cardElement = newCard.generateCard();
-    baseCardSection.addItem(cardElement);
-  }
-}, cardsContainerSelector);
-
-baseCardSection.renderAll();
 
 // Открытие и закрытие окна редактирования профиля
 buttonEditProfile.addEventListener('click', function(){
--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -1,22 +1,37 @@
+// Список селекторов и констант
 const popup = 'popup';
 const popupOpened = 'popup_opened';
 const popupCloseButton = 'popup__close-button';
-const caption = document.querySelector(".popup__figcaption");
-const scaleImage = document.querySelector('.popup__scale-image');
+const cardsContainerSelector = '.elements';
 const formSelector = '.popup__form'
 const inputSelector = '.popup__input';
 const popupEditionSelector = '.popup_type_edition';
 const popupNewCardSelector = '.popup_type_new-card';
 const popupPhotoSelector = '.popup_type_photo';
+
+// Поиск DOM-элементов
+const caption = document.querySelector(".popup__figcaption");
+const scaleImage = document.querySelector('.popup__scale-image');
 const nameUser = document.querySelector('.profile__info-name');
 const professionUser = document.querySelector('.profile__info-profession');
 const nameInput = document.querySelector('.popup__input_type_name');
 const professionInput = document.querySelector('.popup__input_type_profession');
-const cardsContainerSelector = '.elements';
+const buttonEditProfile = document.querySelector(".profile__edit-button");
+const buttonAddCard = document.querySelector(".profile__add-button");
+
+// Объект настроек с селекторами
+const validationVariables = {
+  inputSelector: '.popup__input',
+  inputErrorClass: 'popup__input_error',
+  saveButtonSelector: '.popup__save-button',
+  saveButtonDisabledClass: 'popup__save-button_disabled',
+  spanErrorClass: 'popup__error_visible',
+  popupErrorTypeSelector: '.popup__error_type_'
+};
 
 export { popupOpened, popup, popupCloseButton, caption, scaleImage, formSelector, inputSelector,
   popupEditionSelector, popupNewCardSelector, popupPhotoSelector, nameUser, professionUser,
-  nameInput, professionInput, cardsContainerSelector }
+  nameInput, professionInput, cardsContainerSelector, buttonAddCard, buttonEditProfile, validationVariables }
 
 
 
